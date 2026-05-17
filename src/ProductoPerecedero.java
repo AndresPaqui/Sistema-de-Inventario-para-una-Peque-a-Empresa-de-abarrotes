017f8b137aa908f0f6b84d980138b2326245152c
LeonardoTapia7 <joel.tapia@udla.edu.ec>
2026-05-17 18:23:44 -0500

    Add getters and setters for date fields

--- src/ProductoPerecedero.java
+++ src/ProductoPerecedero.java
@@ -13,4 +13,28 @@ public class ProductoPerecedero extends Producto{
         this.mes = mes;
         this.año = año;
     }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
 }
