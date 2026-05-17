af7fcf6e5217dd6aa5e5ce5f1a708d94181a24d0
LeonardoTapia7 <joel.tapia@udla.edu.ec>
2026-05-17 18:25:10 -0500

    Update Inventario.java
    Add comment explaining the behavior of the eliminar method.

--- src/Inventario.java
+++ src/Inventario.java
@@ -6,7 +6,7 @@ public class Inventario {
     private List<Producto> listaProductos = new ArrayList<>();
 
     public int agregar(Producto p) {listaProductos.add(p);return 1;}
-
+//Cuando lo encuentra, remove(i) lo borra de la lista y retorna 1 (éxito). Si recorre toda la lista y no encuentra nada, retorna 0 (no existe).
     public int eliminar(int id) {
         for (int i = 0; i < listaProductos.size(); i++) {
             if (listaProductos.get(i).getID() == id) {
