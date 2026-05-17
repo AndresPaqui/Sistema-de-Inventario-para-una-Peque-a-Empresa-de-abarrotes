d7b0a6928ff127082551d298f333148ec4b0259b
LeonardoTapia7 <joel.tapia@udla.edu.ec>
2026-05-17 18:20:30 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
+        Inventario inventario  = new Inventario();
+        DatosMiau controlador = new DatosMiau(inventario);
+        Interfaz interfaz      = new Interfaz(controlador);
+        interfaz.mostrar();
     }
 }
